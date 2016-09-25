1dfc2dcbe31d81d5502b83ba6c9a413deb21e827
Chris <jc.honkanen@gmail.com>
2016-09-25 18:00:45 +0100

    Set Up Unit Test

---
+++ UnitTest/CsvParse.cpp
@@ -0,0 +1,16 @@
+#include "stdafx.h"
+#include "CppUnitTest.h"
+
+using namespace Microsoft::VisualStudio::CppUnitTestFramework;
+
+namespace UnitTest
+{		
+	TEST_CLASS(CsvParse)
+	{
+	public:
+		TEST_METHOD(OpenAndReadFile)
+		{
+			// TODO: Your test code here
+		}
+	};
+}
